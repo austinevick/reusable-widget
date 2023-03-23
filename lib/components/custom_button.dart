cd391311f5d6a64a055a8daf627de384b05df5ae
Augustine Victor <augustinevickky@gmail.com>
2023-03-23 15:03:37 -0700

    Update custom_button.dart

--- lib/components/custom_button.dart
+++ lib/components/custom_button.dart
@@ -7,42 +7,56 @@ class CustomButton extends StatelessWidget {
   final Color? color;
   final double? width;
   final double? height;
-  final TextStyle? textStyle;
+  final Color? textColor;
   final List<Color>? gradient;
   final BorderSide? borderSide;
+  final double? radius;
+  final double? textSize;
+  final bool isLoading;
+  final BorderRadius? borderRadius;
   const CustomButton(
       {Key? key,
       this.onPressed,
-      this.textStyle,
+      this.textColor,
       this.text,
       this.color,
       this.child,
       this.width = double.infinity,
       this.height,
       this.gradient = const [Colors.transparent, Colors.transparent],
-      this.borderSide})
+      this.borderSide,
+      this.radius = 50,
+      this.borderRadius,
+      this.isLoading = false,
+      this.textSize})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: height ?? 50,
-      width: width,
-      child: Container(
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(8),
-            gradient: LinearGradient(colors: gradient!)),
-        child: MaterialButton(
-          onPressed: onPressed,
-          child: child ??
-              Text(text!,
-                  style: textStyle ??
-                      const TextStyle(
-                          color: Colors.white, fontWeight: FontWeight.w600)),
-          color: color,
-          shape: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(8),
-              borderSide: borderSide ?? BorderSide.none),
+    return IgnorePointer(
+      ignoring: isLoading ? true : false,
+      child: SizedBox(
+        height: height ?? 60,
+        width: width,
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 650),
+          decoration: BoxDecoration(
+              borderRadius: borderRadius ?? BorderRadius.circular(radius!),
+              gradient: LinearGradient(colors: gradient!)),
+          child: MaterialButton(
+            elevation: 0,
+            onPressed: onPressed,
+            color: color,
+            shape: OutlineInputBorder(
+                borderRadius: borderRadius ?? BorderRadius.circular(radius!),
+                borderSide: borderSide ?? BorderSide.none),
+            child: child ??
+                Text(text!,
+                    style: TextStyle(
+                        color: textColor ?? Colors.white,
+                        fontSize: textSize ?? 17,
+                        fontWeight: FontWeight.w600)),
+          ),
         ),
       ),
     );
