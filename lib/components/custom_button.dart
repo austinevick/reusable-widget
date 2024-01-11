75876388dbee34891934193366f0fa2e911b8a35
Augustine Victor <augustinevickky@gmail.com>
2024-01-11 11:57:57 +0100

    Update custom_button.dart

--- lib/components/custom_button.dart
+++ lib/components/custom_button.dart
@@ -63,3 +63,42 @@ class CustomButton extends StatelessWidget {
     );
   }
 }
+
+class ButtonLoader extends StatelessWidget {
+  final bool isLoading;
+  final String? text;
+  final Color? color;
+  final Color? spinnerColor;
+  const ButtonLoader(
+      {Key? key,
+      required this.isLoading,
+      required this.text,
+      this.color,
+      this.spinnerColor})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return !isLoading
+        ? Text(
+            text!,
+            style: TextStyle(
+              color: color,
+            ),
+          )
+        : Center(
+            child: SizedBox(
+                height: 20,
+                width: 20,
+                child: CircularProgressIndicator(
+                    color: spinnerColor ?? Colors.white, strokeWidth: 2)),
+          );
+  }
+}
+
+
+
+
+
+
+
